1d0e39986835acc61497ff593e80c98f3308bfc6
Peter Polidoro <peter@polidoro.io>
2024-07-29 14:30:34 -0400

    Uart -> UART

--- examples/UART/ReadIoin/ReadIoin.ino
+++ examples/UART/ReadIoin/ReadIoin.ino
@@ -1,16 +1,19 @@
 #include <TMC51X0.hpp>
 
 
-#if defined(ARDUINO_ARCH_RP2040)
+#if !defined(ARDUINO_ARCH_RP2040)
+HardwareSerial & uart = Serial1;
+#else
+SerialUART & uart = Serial1;
 pin_size_t TX_PIN = 4;
 pin_size_t RX_PIN = 5;
 #endif
 
-HardwareSerial & uart = Serial1;
-
-const uint8_t CHIP_SELECT_PIN = 10;
-
-const uint8_t ENABLE_HARDWARE_PIN = 4;
+// ENABLE_TX_PIN and ENABLE_RX_PIN may be the same pin
+const uint8_t ENABLE_TX_PIN = 15;
+const uint8_t ENABLE_RX_PIN = 14;
+const uint8_t ENABLE_TX_POLARITY = HIGH;
+const uint8_t ENABLE_RX_POLARITY = LOW;
 
 const long SERIAL_BAUD_RATE = 115200;
 const int DELAY = 1000;
@@ -27,7 +30,7 @@ void setup()
   uart.setRX(RX_PIN);
 #endif
 
-  tmc5160.setup(uart, CHIP_SELECT_PIN);
+  tmc5160.setup(uart, ENABLE_TX_PIN, ENABLE_RX_PIN, ENABLE_TX_POLARITY, ENABLE_RX_POLARITY);
   tmc5160.driver.setEnableHardwarePin(ENABLE_HARDWARE_PIN);
 
   enabled = false;
