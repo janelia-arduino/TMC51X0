9573c65e37d7935266bee8cf2afd30936c8a81a2
Peter Polidoro <peter@polidoro.io>
2024-11-18 10:08:02 -0500

    Modify Interface to allow debug interface to work

--- src/TMC51X0/Interface.hpp
+++ src/TMC51X0/Interface.hpp
@@ -21,9 +21,9 @@ public:
   };
   InterfaceMode interface_mode;
 
-  virtual void writeRegister(uint8_t register_address,
-    uint32_t data);
-  virtual uint32_t readRegister(uint8_t register_address);
+  virtual void writeRegister(uint8_t /* register_address */,
+    uint32_t /* data */){};
+  virtual uint32_t readRegister(uint8_t /* register_address */){return 0;};
 };
 }
 #endif
