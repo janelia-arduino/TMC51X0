06feafa54b8d7e3e13a67cbb1f5e406152110c77
Peter Polidoro <peter@polidoro.io>
2025-12-10 13:42:43 -0500

    Reformat UartParameters

--- src/UartParameters.hpp
+++ src/UartParameters.hpp
@@ -7,36 +7,51 @@
 
 #ifndef TMC51X0_UART_PARAMETERS_HPP
 #define TMC51X0_UART_PARAMETERS_HPP
-#include <Arduino.h>
 
+#include <Arduino.h>
 
 namespace tmc51x0
 {
 struct UartParameters
 {
   Stream * uart_ptr;
-  uint8_t node_address;
-  size_t enable_txrx_pin;
+  uint8_t  node_address;
+  size_t   enable_txrx_pin;
 
-  constexpr UartParameters(Stream *s = nullptr,
-                           uint8_t na = 0,
-                           size_t en = false)
-  : uart_ptr(s), node_address(na), enable_txrx_pin(en) {}
+  constexpr UartParameters(Stream *uart_ptr        = nullptr,
+                           uint8_t node_address    = 0,
+                           size_t  enable_txrx_pin = 0)
+  : uart_ptr(uart_ptr),
+    node_address(node_address),
+    enable_txrx_pin(enable_txrx_pin)
+  {}
 
-  constexpr UartParameters withUart(Stream *s) const
+  // "Named parameter" style helpers
+
+  constexpr UartParameters withUart(Stream *uart) const
   {
-    return UartParameters{s, node_address, enable_txrx_pin};
+    return UartParameters(
+      uart,
+      node_address,
+      enable_txrx_pin);
   }
 
   constexpr UartParameters withNodeAddress(uint8_t na) const
   {
-    return UartParameters{uart_ptr, na, enable_txrx_pin};
+    return UartParameters(
+      uart_ptr,
+      na,
+      enable_txrx_pin);
   }
 
   constexpr UartParameters withEnableTxRxPin(size_t en) const
   {
-    return UartParameters{uart_ptr, node_address, en};
+    return UartParameters(
+      uart_ptr,
+      node_address,
+      en);
   }
 };
-}
-#endif
+} // namespace tmc51x0
+
+#endif // TMC51X0_UART_PARAMETERS_HPP
