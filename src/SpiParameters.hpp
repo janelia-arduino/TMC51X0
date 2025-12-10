0a4e28739b6fd08364300fd30bd3480ca3eb93af
Peter Polidoro <peter@polidoro.io>
2025-12-10 13:02:25 -0500

    Reformat SpiParameters

--- src/SpiParameters.hpp
+++ src/SpiParameters.hpp
@@ -7,43 +7,61 @@
 
 #ifndef TMC51X0_SPI_PARAMETERS_HPP
 #define TMC51X0_SPI_PARAMETERS_HPP
-#include <SPI.h>
 
+#include <SPI.h>
 
 namespace tmc51x0
 {
   struct SpiParameters
 {
   SPIClass * spi_ptr;
-  uint32_t clock_rate;
-  size_t chip_select_pin;
+  uint32_t   clock_rate;
+  size_t     chip_select_pin;
 
-#if defined(ARDUINO_ARCH_SAMD) || defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_ARCH_RENESAS) || defined(ARDUINO_ARCH_MBED)
-  const static BitOrder bit_order = MSBFIRST;
+#if defined(ARDUINO_ARCH_SAMD)    || \
+defined(ARDUINO_ARCH_RP2040)  || \
+defined(ARDUINO_ARCH_RENESAS) || \
+defined(ARDUINO_ARCH_MBED)
+  static const BitOrder bit_order = MSBFIRST;
 #else
-  const static uint8_t bit_order = MSBFIRST;
+  static const uint8_t bit_order  = MSBFIRST;
 #endif
-  const static uint8_t data_mode = SPI_MODE3;
+  static const uint8_t data_mode  = SPI_MODE3;
 
-  constexpr SpiParameters(SPIClass *s = nullptr,
-                          uint32_t hz = 1000000,
-                          size_t cs = 0)
-  : spi_ptr(s), clock_rate(hz), chip_select_pin(cs) {}
+  constexpr SpiParameters(SPIClass *spi_ptr          = nullptr,
+                          uint32_t   clock_rate      = 1000000,
+                          size_t     chip_select_pin = 0)
+  : spi_ptr(spi_ptr),
+    clock_rate(clock_rate),
+    chip_select_pin(chip_select_pin)
+  {}
 
-  constexpr SpiParameters withSpi(SPIClass *s) const
+  // "Named parameter" style helpers
+
+  constexpr SpiParameters withSpi(SPIClass *spi) const
   {
-    return SpiParameters{s, clock_rate, chip_select_pin};
+    return SpiParameters(
+                         spi,
+                         clock_rate,
+                         chip_select_pin);
   }
 
   constexpr SpiParameters withClockRate(uint32_t hz) const
   {
-    return SpiParameters{spi_ptr, hz, chip_select_pin};
+    return SpiParameters(
+                         spi_ptr,
+                         hz,
+                         chip_select_pin);
   }
 
   constexpr SpiParameters withChipSelectPin(size_t cs) const
   {
-    return SpiParameters{spi_ptr, clock_rate, cs};
+    return SpiParameters(
+                         spi_ptr,
+                         clock_rate,
+                         cs);
   }
 };
-}
-#endif
+} // namespace tmc51x0
+
+#endif // TMC51X0_SPI_PARAMETERS_HPP
